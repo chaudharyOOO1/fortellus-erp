ee7b30ddd47cb1c8f94419a6f118592c10d1fc5b
chaudharyOOO1 <67969881+chaudharyOOO1@users.noreply.github.com>
2026-09-22 23:10:53 +0530

    Create CRUD base

---
+++ backend/app/crud/base.py
@@ -0,0 +1,44 @@
+from typing import Any, Generic, List, Optional, Type, TypeVar, Union
+from sqlalchemy.orm import Session
+from pydantic import BaseModel
+
+ModelType = TypeVar("ModelType")
+CreateSchemaType = TypeVar("CreateSchemaType", bound=BaseModel)
+UpdateSchemaType = TypeVar("UpdateSchemaType", bound=BaseModel)
+
+class CRUDBase(Generic[ModelType, CreateSchemaType, UpdateSchemaType]):
+    def __init__(self, model: Type[ModelType]):
+        self.model = model
+
+    def get(self, db: Session, id: Any) -> Optional[ModelType]:
+        return db.query(self.model).filter(self.model.id == id).first()
+
+    def get_multi(self, db: Session, *, skip: int = 0, limit: int = 100) -> List[ModelType]:
+        return db.query(self.model).offset(skip).limit(limit).all()
+
+    def create(self, db: Session, *, obj_in: CreateSchemaType) -> ModelType:
+        obj_data = obj_in.model_dump()
+        db_obj = self.model(**obj_data)
+        db.add(db_obj)
+        db.commit()
+        db.refresh(db_obj)
+        return db_obj
+
+    def update(self, db: Session, *, db_obj: ModelType, obj_in: Union[UpdateSchemaType, dict]) -> ModelType:
+        data = obj_in if isinstance(obj_in, dict) else obj_in.model_dump(exclude_unset=True)
+        for field, value in data.items():
+            if field == "password":
+                continue
+            setattr(db_obj, field, value)
+        db.add(db_obj)
+        db.commit()
+        db.refresh(db_obj)
+        return db_obj
+
+    def remove(self, db: Session, *, id: int) -> ModelType:
+        obj = db.query(self.model).get(id)
+        if obj is None:
+            return None
+        db.delete(obj)
+        db.commit()
+        return obj

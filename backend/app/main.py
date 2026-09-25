dc877da4321adeac250fe562b4d9b26030313a8c
chaudharyOOO1 <67969881+chaudharyOOO1@users.noreply.github.com>
2026-09-25 23:45:04 +0530

    Support Vercel frontend deployment CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -16,7 +16,7 @@ app = FastAPI(
 if settings.BACKEND_CORS_ORIGINS:
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=settings.BACKEND_CORS_ORIGINS,
+        allow_origins=settings.BACKEND_CORS_ORIGINS,\n        allow_origin_regex=settings.BACKEND_CORS_ORIGIN_REGEX,
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
